463def9f8cd0600f2d7f27d433deb97a7829e7bf
khushi <patel559@uwindsor.ca>
2024-06-18 23:40:27 -0400

    18 june update

--- guides/admin.py
+++ guides/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CompostingTip, UpcyclingProject
 
 # Register your models here.
+admin.site.register(CompostingTip)
+admin.site.register(UpcyclingProject)
