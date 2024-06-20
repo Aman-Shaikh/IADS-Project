997323061bfbfb16c5e8ee6ddac609caba2acff1
Aman Shaikh <amanshaikh@Amans-MacBook-Air.local>
2024-06-20 19:09:16 -0400

    Changed layout

--- RecycleWise/urls.py
+++ RecycleWise/urls.py
@@ -6,10 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('user/', include('user.urls')),
-    path('guides/', include('guides.urls')),
-    # path('search/', include('search.urls')),
-    # path('contact/', include('contact.urls')),
-    # path('recycling-centers/', include('recycling_centers.urls')),
-    path('uploads/', include('uploads.urls')),
 ]
--- guides/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('guide_submission/', views.guide_submission, name='guide_submission'),
-    # path('composting_tips/', views.composting_tips, name='composting_tips'),
-    # path('upcycling_project/', views.upcycling_project, name='upcycling_project'),
-]
