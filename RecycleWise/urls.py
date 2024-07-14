f378bdb034b8ed548e8ecd934a599f288feeb903
Manav Patel <PATEL359@UWINDSOR.CA>
2024-07-14 01:37:56 -0400

    upcomming_events- manav

--- RecycleWise/urls.py
+++ RecycleWise/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
     path('', include('getting_started.urls')),
-    path('upcoming_events/', include('upcoming_events.urls')),
+    path('upcoming_events/', include('upcomming_events.urls')),
     # path('', include('userfeedback.urls')),
     path('interactivetools/', include('interactivetools.urls')),
 
--- upcomming_events/views.py
+++ upcomming_events/views.py
@@ -9,25 +9,25 @@ from .forms import (
 # Base views for events
 class EventListView(ListView):
     model = Event
-    template_name = 'upcoming_events/event_list.html'
+    template_name = 'upcomming_events/event_list.html'
     context_object_name = 'events'
 
 class EventDetailView(DetailView):
     model = Event
-    template_name = 'upcoming_events/event_detail.html'
+    template_name = 'upcomming_events/event_detail.html'
     context_object_name = 'event'
 
 class EventCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Event
-    template_name = 'upcoming_events/event_form.html'
-    success_url = '/upcoming_events/'
+    template_name = 'upcomming_events/event_form.html'
+    success_url = '/upcomming_events/'
 
     def test_func(self):
         return self.request.user.is_superuser
 
 class RSVPCreateView(LoginRequiredMixin, CreateView):
     model = RSVP
-    template_name = 'upcoming_events/rsvp_form.html'
+    template_name = 'upcomming_events/rsvp_form.html'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -40,29 +40,29 @@ class RSVPCreateView(LoginRequiredMixin, CreateView):
 # Community Clean-Up views
 class CommunityCleanUpListView(EventListView):
     queryset = Event.objects.filter(event_type='CCU')
-    template_name = 'upcoming_events/community_cleanup_list.html'
+    template_name = 'upcomming_events/community_cleanup_list.html'
 
 class CommunityCleanUpDetailView(EventDetailView):
-    template_name = 'upcoming_events/community_cleanup_detail.html'
+    template_name = 'upcomming_events/community_cleanup_detail.html'
 
 class CommunityCleanUpCreateView(EventCreateView):
     form_class = CommunityCleanUpForm
 
 class CommunityCleanUpRSVPView(RSVPCreateView):
     form_class = CommunityCleanUpRSVPForm
-    template_name = 'upcoming_events/community_cleanup_rsvp_form.html'
+    template_name = 'upcomming_events/community_cleanup_rsvp_form.html'
 
 # Recycling Workshop views
 class RecyclingWorkshopListView(EventListView):
     queryset = Event.objects.filter(event_type='RW')
-    template_name = 'upcoming_events/recycling_workshop_list.html'
+    template_name = 'upcomming_events/recycling_workshop_list.html'
 
 class RecyclingWorkshopDetailView(EventDetailView):
-    template_name = 'upcoming_events/recycling_workshop_detail.html'
+    template_name = 'upcomming_events/recycling_workshop_detail.html'
 
 class RecyclingWorkshopCreateView(EventCreateView):
     form_class = RecyclingWorkshopForm
 
 class RecyclingWorkshopRSVPView(RSVPCreateView):
     form_class = RecyclingWorkshopRSVPForm
-    template_name = 'upcoming_events/recycling_workshop_rsvp_form.html'
+    template_name = 'upcomming_events/recycling_workshop_rsvp_form.html'
