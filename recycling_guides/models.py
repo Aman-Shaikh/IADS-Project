ae255d5b24675f1775d9410d5e85c74edd0ff1cf
Yahya9870 <56015489+Yahya9870@users.noreply.github.com>
2024-07-20 18:19:24 -0400

    UI Changes and data added in recycling guides section

--- recycling_guides/models.py
+++ recycling_guides/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class RecyclingGuide(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='recycling_guides/')
+    image = models.ImageField(upload_to='static/images/recycling_guides/')
     description = models.TextField()
     detailed_description = models.TextField()
 
