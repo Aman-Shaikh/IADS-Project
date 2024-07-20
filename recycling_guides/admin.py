e48b0368019049654a3e034edc54058774b0fb01
Yahya9870 <56015489+Yahya9870@users.noreply.github.com>
2024-07-20 18:06:29 -0400

    admin section for guides added

--- recycling_guides/admin.py
+++ recycling_guides/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import RecyclingGuide
 
-# Register your models here.
+
+class RecyclingGuideAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image_tag')
+    search_fields = ('title', 'description')
+    readonly_fields = ('image_tag',)
+
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="50" height="50" />'.format(obj.image.url))
+        return None
+
+    image_tag.short_description = 'Image'
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'image', 'image_tag', 'description', 'detailed_description')
+        }),
+    )
+
+
+admin.site.register(RecyclingGuide, RecyclingGuideAdmin)
