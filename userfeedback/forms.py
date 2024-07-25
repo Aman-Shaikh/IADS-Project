7e4b7145bcb6e59e877c934e3e08247a46379174
shehreyarwaqar45 <136549191+shehreyarwaqar45@users.noreply.github.com>
2024-07-25 05:40:33 -0400

    Update the Form to use ModelForm

--- userfeedback/forms.py
+++ userfeedback/forms.py
@@ -1,11 +1,19 @@
 from django import forms
+from .models import Complaint
 
+class ComplaintForm(forms.ModelForm):
+    class Meta:
+        model = Complaint
+        fields = ['name', 'email', 'phone', 'complaint']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First and Last'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'email@domain.tld'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '(555) 555-5555'}),
+            'complaint': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your Complaint here.....', 'rows': 6}),
+        }
 
-
-class ComplaintForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First and Last'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'email@domain.tld'}))
-    phone = forms.CharField(max_length=15, required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '(555) 555-5555'}))
-    complaint = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your Complaint here.....', 'rows': 6}))
-
-
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if phone and not phone.isdigit():
+            raise forms.ValidationError("Phone number must contain only digits.")
+        return phone
