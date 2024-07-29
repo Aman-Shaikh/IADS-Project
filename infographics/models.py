f61bfce1888fd02466c67a00eff4577b11fe6513
Yahya9870 <56015489+Yahya9870@users.noreply.github.com>
2024-07-29 02:36:32 -0400

    Infographics models created

---
+++ infographics/models.py
@@ -0,0 +1,12 @@
+# infographics/models.py
+
+from django.db import models
+
+class Infographic(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='infographics/')
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
