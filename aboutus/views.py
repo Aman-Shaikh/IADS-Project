273b205e75a7741d50ea75c903374281bbdf545e
Aman Shaikh <85395708+Aman-Shaikh@users.noreply.github.com>
2024-07-29 05:12:44 -0400

    views created for aboutus

--- aboutus/views.py
+++ aboutus/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import TeamMember, Section
+from .forms import TeamMemberForm, SectionForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def team_member_list(request):
+    members = TeamMember.objects.all()
+    return render(request, 'aboutus/team_member_list.html', {'members': members})
+
+@login_required
+def team_member_detail(request, pk):
+    member = get_object_or_404(TeamMember, pk=pk)
+    return render(request, 'aboutus/team_member_detail.html', {'member': member})
+
+@login_required
+def section_list(request):
+    sections = Section.objects.all()
+    return render(request, 'aboutus/section_list.html', {'sections': sections})
+
+@login_required
+def section_detail(request, pk):
+    section = get_object_or_404(Section, pk=pk)
+    return render(request, 'aboutus/section_detail.html', {'section': section})
