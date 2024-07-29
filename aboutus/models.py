706136772010f99ff2cb7933e082c00dc752d724
Aman Shaikh <85395708+Aman-Shaikh@users.noreply.github.com>
2024-07-29 05:11:11 -0400

    models for aboutus

--- aboutus/models.py
+++ aboutus/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Section(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class TeamMember(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    sections = models.ManyToManyField(Section, related_name='team_members')
+
+    def __str__(self):
+        return self.user.username
