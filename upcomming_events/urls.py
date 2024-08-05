e7c27525c544a96eaf3494ad1c6de09d8ccba5a2
Manav Patel <PATEL359@UWINDSOR.CA>
2024-08-05 17:11:16 -0400

    final touch

--- upcomming_events/urls.py
+++ upcomming_events/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 from .views import (
-    CommunityCleanUpListView, CommunityCleanUpDetailView, CommunityCleanUpRSVPView, RecyclingWorkshopListView, RecyclingWorkshopDetailView, RecyclingWorkshopRSVPView, WorkshopFeedbackView
+    community_cleanup_list,
+    community_cleanup_detail,
+    community_cleanup_rsvp,
+    recycling_workshop_list,
+    recycling_workshop_detail,
+    recycling_workshop_rsvp,
 )
 
 urlpatterns = [
-    path('', CommunityCleanUpListView.as_view(), name='community-cleanup-list'),
-    path('community_cleanup/<int:pk>/', CommunityCleanUpDetailView.as_view(), name='community-cleanup-detail'),
-    path('community_cleanup/<int:pk>/rsvp/', CommunityCleanUpRSVPView.as_view(), name='community-cleanup-rsvp'),
+    path('', community_cleanup_list, name='community-cleanup-list'),
+    path('community_cleanup/<int:pk>/', community_cleanup_detail, name='community-cleanup-detail'),
+    path('community_cleanup/<int:pk>/rsvp/', community_cleanup_rsvp, name='community-cleanup-rsvp'),
 
-    path('recycling_workshop/', RecyclingWorkshopListView.as_view(), name='recycling-workshop-list'),
-    path('recycling_workshop/<int:pk>/', RecyclingWorkshopDetailView.as_view(), name='recycling-workshop-detail'),
-    path('recycling_workshop/<int:pk>/rsvp/', RecyclingWorkshopRSVPView.as_view(), name='recycling-workshop-rsvp'),
-    path('recycling_workshop/<int:pk>/feedback/', WorkshopFeedbackView.as_view(), name='workshop-feedback'),
+    path('recycling_workshop/', recycling_workshop_list, name='recycling-workshop-list'),
+    path('recycling_workshop/<int:pk>/', recycling_workshop_detail, name='recycling-workshop-detail'),
+    path('recycling_workshop/<int:pk>/rsvp/', recycling_workshop_rsvp, name='recycling-workshop-rsvp'),
+    # path('recycling_workshop/<int:pk>/feedback/', workshop_feedback, name='workshop-feedback'),
 ]
